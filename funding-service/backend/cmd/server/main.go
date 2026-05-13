0fa7a6cec472f8cf01e454a9eab79399cd731049
Bogdan Dranicin <bdranitsyn@synergy.ru>
2026-05-13 19:11:57 +0300

    feat: add main.go with zerolog startup (task 0.2)

---
+++ funding-service/backend/cmd/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/funding-service/backend/internal/config"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal().Err(err).Msg("config load failed")
+	}
+
+	level, err := zerolog.ParseLevel(cfg.LogLevel)
+	if err != nil {
+		level = zerolog.InfoLevel
+	}
+	zerolog.SetGlobalLevel(level)
+	log.Logger = log.Output(os.Stdout)
+
+	log.Info().
+		Int("port", cfg.Port).
+		Int("moex_poll_ms", cfg.MOEXPollMs).
+		Str("log_level", cfg.LogLevel).
+		Msg("service starting")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info().Msg("shutting down")
+}
--- funding-service/backend/go.mod
+++ funding-service/backend/go.mod
@@ -2,4 +2,14 @@ module github.com/funding-service/backend
 
 go 1.26.3
 
-require github.com/kelseyhightower/envconfig v1.4.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/rs/zerolog v1.35.1
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
